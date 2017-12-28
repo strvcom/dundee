4871d8dc8d67f9006edece0cfb0892138586d5d1
dostalleos <dostalleos@gmail.com>
2017-12-28 16:27:48 +0100

    Firestore test WIP

--- app/src/main/kotlin/com/strv/dundee/firestore/Firestore_test.kt
+++ app/src/main/kotlin/com/strv/dundee/firestore/Firestore_test.kt
@@ -1,6 +1,7 @@
 package com.strv.dundee.firestore
 
-import com.google.firebase.firestore.DocumentReference
+import android.annotation.SuppressLint
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 import com.strv.ktools.logD
@@ -8,10 +9,10 @@ import com.strv.ktools.logMeD
 
 
 object Firestore {
+	@SuppressLint("StaticFieldLeak")
+	val db = FirebaseFirestore.getInstance()
 
 	fun read() {
-
-		val db = FirebaseFirestore.getInstance()
 
 		db.collection("users")
 				.get()
@@ -27,9 +28,7 @@ object Firestore {
 	}
 
 	fun set(collection: String, data: HashMap<String, Any?>, documentId: String? = null, merge: Boolean = false) {
-		val db = FirebaseFirestore.getInstance()
-		val documentRef: DocumentReference
-		documentRef = if (documentId != null) db.collection(collection).document(documentId) else db.collection(collection).document()
+		val documentRef = if (documentId != null) db.collection(collection).document(documentId) else db.collection(collection).document()
 		val docId = documentRef.id
 		val task = if(merge) documentRef.set(data, SetOptions.merge()) else documentRef.set(data)
 		task.addOnSuccessListener { logD("Success, collection $collection documentID = $docId") }
@@ -37,9 +36,7 @@ object Firestore {
 	}
 
 	fun set(collection: String, data: Any, documentId: String? = null, merge: Boolean = false) {
-		val db = FirebaseFirestore.getInstance()
-		val documentRef: DocumentReference
-		documentRef = if (documentId != null) db.collection(collection).document(documentId) else db.collection(collection).document()
+		val documentRef = if (documentId != null) db.collection(collection).document(documentId) else db.collection(collection).document()
 		val docId = documentRef.id
 		val task = if(merge) documentRef.set(data, SetOptions.merge()) else documentRef.set(data)
 		task.addOnSuccessListener { logD("Set success, collection $collection documentID = $docId") }
@@ -47,10 +44,25 @@ object Firestore {
 	}
 
 	fun updateField(collection: String, documentId: String, field: String, value: Any?) {
-		val db = FirebaseFirestore.getInstance()
 		val documentRef = db.collection(collection).document(documentId)
 		documentRef.update(field, value)
 				.addOnSuccessListener { logD("Update field success, collection $collection documentId = $documentId") }
+				.addOnFailureListener { e -> e.logMeD() }
+	}
+
+	fun deleteDocument(collection: String, documentId: String) {
+		val documentRef = db.collection(collection).document(documentId)
+		documentRef.delete()
+				.addOnSuccessListener { logD("Delete success, collection $collection documentId = $documentId") }
+				.addOnFailureListener { e -> e.logMeD() }
+	}
+
+	fun deleteField(collection: String, documentId: String, field: String) {
+		val documentRef = db.collection(collection).document(documentId)
+		val update = HashMap<String, Any>()
+		update.put(field, FieldValue.delete())
+		documentRef.update(update)
+				.addOnSuccessListener { logD("Delete field success, collection $collection documentId = $documentId") }
 				.addOnFailureListener { e -> e.logMeD() }
 	}
 }
