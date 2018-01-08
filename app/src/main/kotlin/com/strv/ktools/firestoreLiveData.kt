b466a017e00f5c86aab28c800a91ce1c953cafcf
Jakub Kinst <jakub@kinst.cz>
2018-01-08 16:50:07 +0100

    Fixed last commit

---
+++ app/src/main/kotlin/com/strv/ktools/firestoreLiveData.kt
@@ -0,0 +1,81 @@
+package com.strv.ktools
+
+import android.arch.lifecycle.LiveData
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import com.strv.dundee.model.repo.common.Resource
+import com.strv.dundee.model.repo.common.Status
+
+class FirestoreDocumentLiveData<T>(private val documentRef: DocumentReference, private val clazz: Class<T>) : LiveData<Resource<T>>() {
+
+	// if cached data are up to date with server DB, listener won't get called again with isFromCache=false
+	private val listener = EventListener<DocumentSnapshot> { value, e ->
+		logD("Loaded ${documentRef.path}, cache: ${value?.metadata?.isFromCache.toString()} error: ${e?.message}")
+		setValue(Resource(if (e != null) Status.ERROR else if (value.metadata.isFromCache) Status.LOADING else Status.SUCCESS,
+				value?.toObject(clazz), e?.message))
+	}
+	private lateinit var listenerRegistration: ListenerRegistration
+
+	override fun onActive() {
+		super.onActive()
+		logD("Start listening ${documentRef.path}")
+		listenerRegistration = documentRef.addSnapshotListener(listener)
+	}
+
+	override fun onInactive() {
+		super.onInactive()
+		logD("Stop listening ${documentRef.path}")
+		listenerRegistration.remove()
+	}
+}
+
+class FirestoreDocumentQueryLiveData<T>(private val query: Query, private val clazz: Class<T>) : LiveData<Resource<T>>() {
+
+	// if cached data are up to date with server DB, listener won't get called again with isFromCache=false
+	private val listener = EventListener<QuerySnapshot> { value, e ->
+		logD("Loaded $query, cache: ${value?.metadata?.isFromCache.toString()} error: ${e?.message}")
+		val list = value?.toObjects(clazz)
+		setValue(Resource(if (e != null) Status.ERROR else if (value.metadata.isFromCache) Status.LOADING else Status.SUCCESS,
+				if (list != null && !list.isEmpty()) list[0] else null, e?.message))
+	}
+	private lateinit var listenerRegistration: ListenerRegistration
+
+	override fun onActive() {
+		super.onActive()
+		logD("Start listening $query")
+		listenerRegistration = query.limit(1).addSnapshotListener(listener)
+	}
+
+	override fun onInactive() {
+		super.onInactive()
+		logD("Stop listening $query")
+		listenerRegistration.remove()
+	}
+}
+
+class FirestoreDocumentListLiveData<T>(private val query: Query, private val clazz: Class<T>) : LiveData<Resource<List<T>>>() {
+
+	// if cached data are up to date with server DB, listener won't get called again with isFromCache=false
+	private val listener = EventListener<QuerySnapshot> { value, e ->
+		logD("Loaded $query, cache: ${value?.metadata?.isFromCache.toString()} error: ${e?.message}")
+		setValue(Resource(if (e != null) Status.ERROR else if (value.metadata.isFromCache) Status.LOADING else Status.SUCCESS,
+				value?.toObjects(clazz), e?.message))
+	}
+	private lateinit var listenerRegistration: ListenerRegistration
+
+	override fun onActive() {
+		super.onActive()
+		logD("Start listening $query")
+		listenerRegistration = query.addSnapshotListener(listener)
+	}
+
+	override fun onInactive() {
+		super.onInactive()
+		logD("Stop listening $query")
+		listenerRegistration.remove()
+	}
+}
