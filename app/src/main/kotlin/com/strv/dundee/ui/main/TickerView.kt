7acad5b3c2bc5295c44f168fcae07a3596d5e191
Jakub Kinst <jakub@kinst.cz>
2018-01-09 15:51:45 +0100

    Ticker added

---
+++ app/src/main/kotlin/com/strv/dundee/ui/main/TickerView.kt
@@ -0,0 +1,38 @@
+package com.strv.dundee.ui.main
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.strv.dundee.databinding.ItemTickerBinding
+import com.strv.dundee.model.entity.Ticker
+import com.strv.ktools.Resource
+
+
+class TickerView : FrameLayout {
+	constructor(context: Context) : super(context)
+	constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+	constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+	private var layoutInflater: LayoutInflater = LayoutInflater.from(context)
+	private var rootBinding: ItemTickerBinding
+
+	var ticker: Resource<Ticker>? = null
+		set(value) {
+			rootBinding.ticker = value
+		}
+
+	var currency: String? = null
+		set(value) {
+			rootBinding.currency = value
+		}
+
+	var coin: String? = null
+		set(value) {
+			rootBinding.coin = value
+		}
+
+	init {
+		rootBinding = ItemTickerBinding.inflate(layoutInflater, this, true)
+	}
+}
