2541e5547280b1bb6692e52c976f99d6a482a013
Jakub Kinst <jakub@kinst.cz>
2018-01-31 13:07:59 +0100

    Fixed api selection

--- app/src/main/kotlin/com/strv/dundee/model/repo/BitcoinRepository.kt
+++ app/src/main/kotlin/com/strv/dundee/model/repo/BitcoinRepository.kt
@@ -1,24 +1,6 @@
 package com.strv.dundee.model.repo
 
-import com.strv.dundee.model.api.bitfinex.BitfinexApi
-import com.strv.dundee.model.api.bitstamp.BitstampApi
-import com.strv.dundee.model.cache.BitcoinCache
-import com.strv.dundee.model.entity.BitcoinSource
-import com.strv.ktools.inject
-
 class BitcoinRepository {
-	val cache by inject<BitcoinCache>()
-
-	val bitstampApi by inject<BitstampApi>()
-	val bitfinexApi by inject<BitfinexApi>()
-
-	private fun getApi(source: String) = when (source) {
-		BitcoinSource.BITSTAMP -> bitstampApi
-		BitcoinSource.BITFINEX -> bitfinexApi
-		else -> bitstampApi
-	}
-
-	fun getTicker(source: String, coin: String, currency: String) = TickerLiveData(cache, getApi(source)).apply { refresh(source, coin, currency) }
-
-	fun getCandles(source: String, coin: String, currency: String, timeFrame: String) = CandlesLiveData(cache, getApi(source)).apply { refresh(source, coin, currency, timeFrame) }
+	fun getTicker(source: String, coin: String, currency: String) = TickerLiveData().apply { refresh(source, coin, currency) }
+	fun getCandles(source: String, coin: String, currency: String, timeFrame: String) = CandlesLiveData().apply { refresh(source, coin, currency, timeFrame) }
 }
